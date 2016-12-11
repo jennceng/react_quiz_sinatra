2f2a3f7da8d3c822a0510d64a75e591f5f33c136
Jenn Eng <jenneng6@gmail.com>
2016-12-11 15:14:09 -0500

    starting point

--- server.rb
+++ server.rb
@@ -1,4 +1,7 @@
 require "sinatra"
+require "sinatra/json"
+require "json"
+require 'pry'
 
 set :bind, '0.0.0.0'  # bind to all interfaces
 set :public_folder, File.join(File.dirname(__FILE__), "public")
