8b6bb3213709d02048b99e2d62035caff47dfbf0
Jenn Eng <jenneng6@gmail.com>
2016-12-11 15:27:06 -0500

    restructure files, working but no tests yet

---
+++ react/src/components/Answer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Answer = ({ id, body, correct, onClick}) => {
+  return(
+    <div onClick={onClick}>
+      {body}
+    </div>
+  )
+}
+
+export default Answer;
--- react/src/main.js
+++ react/src/main.js
@@ -1,10 +1,13 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
+import data from './constants/data'
+
 
 $(function() {
   ReactDOM.render(
-    <h1>Boo yaa</h1>,
+    <App data={data}/>,
     document.getElementById('app')
   );
 });
